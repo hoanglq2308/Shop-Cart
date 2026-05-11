915615252399859f01917363b9c78a10f6884b05
danhhien <madanhhien7@gmail.com>
2026-05-11 22:20:44 +0700

    fix cartSecurityTest thanh forbidden

--- backend/src/test/java/com/shopcart/controller/CartSecurityTest.java
+++ backend/src/test/java/com/shopcart/controller/CartSecurityTest.java
@@ -1,6 +1,5 @@
 package com.shopcart.controller;
 
-
 import com.shopcart.dto.QuantityUpdateRequest;
 import com.shopcart.dto.CartItemRequest;
 import org.junit.jupiter.api.Test;
@@ -21,42 +20,45 @@ import tools.jackson.databind.ObjectMapper;
 public class CartSecurityTest {
     @Autowired
     private MockMvc mockMvc;
-    @Autowired 
+    @Autowired
     private ObjectMapper objectMapper;
+
     /**
      * Test Case 1: IDOR (Insecure Direct Object Reference)
-     * Mô phỏng: Kẻ tấn công truy cập đổi số lượng Item trong giỏ hàng (cartItemId = 2) 
-     * của người khác mà không có bất kỳ chứng thực nào (Authorization là null hoặc user ảo).
+     * Mô phỏng: Kẻ tấn công truy cập đổi số lượng Item trong giỏ hàng (cartItemId =
+     * 2)
+     * của người khác mà không có bất kỳ chứng thực nào (Authorization là null hoặc
+     * user ảo).
      * BẢO MẬT ĐÚNG: Phải trả về 403 Forbidden hoặc 401 Unauthorized.
      * HIỆN TẠI: Trả về thành công 200 OK (Ứng dụng bị IDOR).
      */
 
     @Test
-    public void TestIDOL_UpdateOtherUserCartIterm() throws Exception{
+    public void TestIDOL_UpdateOtherUserCartIterm() throws Exception {
         QuantityUpdateRequest request = new QuantityUpdateRequest();
         request.setQuantity(5);
         mockMvc.perform(put("/api/cart/2/quantity")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content(objectMapper.writeValueAsString(request)))
-        .andExpect(status().isBadRequest());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
     }
 
     /**
      * Test Case 2: Kiểm tra phân quyền truy cập API / Thiếu CSRF
-     * Mô phỏng: Gửi Fake Request POST từ site độc hại (Attacker) đến thêm sản phẩm vào giỏ.
+     * Mô phỏng: Gửi Fake Request POST từ site độc hại (Attacker) đến thêm sản phẩm
+     * vào giỏ.
      * BẢO MẬT ĐÚNG: Yêu cầu trạng thái 401 hoặc cần token CSRF thì bị chặn báo 403.
-     */@Test
-     public void TEST_CSRF_AddToCartWithOutToken() throws Exception{
+     */
+    @Test
+    public void TEST_CSRF_AddToCartWithOutToken() throws Exception {
         CartItemRequest request = new CartItemRequest();
         request.setProductId("001");
         request.setQuantity(1);
         mockMvc.perform(post("/api/cart/add")
-        .contentType(MediaType.APPLICATION_JSON)
-        .content(objectMapper.writeValueAsString(request)))
-        .andExpect(status().isOk());
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isForbidden());
 
-     }
-
-
+    }
 
 }
